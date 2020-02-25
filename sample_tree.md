e7a669e36dd11bda0b57c61dbc0f542d20f3eeab
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-25 13:08:18 -0500

    tree web page files added

---
+++ sample_tree.md
@@ -0,0 +1,13 @@
+##This Website is about Trees.
+
+-- This website is created with Bootstrap.
+
+-- Navbar is added
+
+-- Card deck and Card body are created with rows and columns.
+
+--With Columns having the tree pictures and giving different information like how huge they are, how weather friendly they are and also how different they are
+
+-- Styled keeping green has the main color of the website 
+
+[Tree page link](https://renujaishankar.github.io/Jan29thRepository/)
---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello there!This script file works")
+const myFunc =() =>{
+   alert("This alert works!")
+}
+function myFunction(){
+   alert("please enter the followin")
+    let name = prompt("Name");
+    let verb = prompt("Verb");
+    console.log("Hello there ${name}}Did you ${verb} today")
+    console.log("Hello there"   +name+" !")
+    var bar=2
+    let x=4.3 + 3
+    let y= "4" + 4 //4
+    let a = 2 * 6
+    let b= 8/2 //0
+    let c = 5 % 2 //1
+    
+    console.log("y"+y)
+ }
+ 
+ let x=4
+ const y = 6
+ var foo  = "this a variable"
+ var script = "ECMAScript"
